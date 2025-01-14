85464aaf76966f7383e7eed70aa8ce04c0bb3e09
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-01-14 23:37:46 +0530

    first genaric method #5

--- Generic types & advanced use of methods/Program.cs
+++ Generic types & advanced use of methods/Program.cs
@@ -122,31 +122,31 @@ Console.WriteLine("Generic Types...............\n\n");
 //        }
 //    }
 //}
-var numbers = new List<int>
-{
-  10,50,20,10,20,4,50,60,20,10,50,20 
-}; 
+//var numbers = new List<int>
+//{
+//  10,50,20,10,20,4,50,60,20,10,50,20 
+//}; 
 
-try
-{
-    var intTuppel = new Tuple<int, int,int>(10,30,40);
-   
-
-    //TwoInput<int,int> twoInput = GetMinAndMax(numbers);
-
-    //Console.WriteLine($"Mininum Value is :{twoInput.Item1}");
-    //Console.WriteLine($"Maxnum Value is :{twoInput.Item2}");
-
-  
+//try
+//{
+//    var intTuppel = new Tuple<int, int,int>(10,30,40);
 
 
-}
-catch (InvalidDataException ex)
-{
-    Console.WriteLine($"Error : {ex.Message}");
-}
+//    //TwoInput<int,int> twoInput = GetMinAndMax(numbers);
 
-Console.ReadLine();
+//    //Console.WriteLine($"Mininum Value is :{twoInput.Item1}");
+//    //Console.WriteLine($"Maxnum Value is :{twoInput.Item2}");
+
+
+
+
+//}
+//catch (InvalidDataException ex)
+//{
+//    Console.WriteLine($"Error : {ex.Message}");
+//}
+
+//Console.ReadLine();
 //TwoInput<int,int> GetMinAndMax(IEnumerable<int> input)
 //{
 //    if (!input.Any())
@@ -170,14 +170,14 @@ Console.ReadLine();
 //        }
 //    }
 //    return new TwoInput<int,int>(Min, Max);
-    
+
 //}
 
 
 
 //public class TwoInput <T1,T2>
 //{
-  
+
 //    public TwoInput(T1 item1, T2 item2)
 //    {
 //        Item1 = item1;
@@ -187,3 +187,27 @@ Console.ReadLine();
 //    public T2 Item2 { get; }
 
 //}
+
+
+//Generic methods.............................................
+
+var number = new List<int> { 1, 5, 6, 8 };
+
+number.AddToFront<int>(10);
+
+
+foreach (var item in number)
+{
+    
+Console.WriteLine(item);
+}
+
+Console.ReadLine();
+
+static class ListExtentions
+{
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+}
