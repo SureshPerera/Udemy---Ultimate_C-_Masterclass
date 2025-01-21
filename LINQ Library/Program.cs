8f03b777681f4336bd587c6f9b8a9892540f2c0b
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-01-21 23:19:26 +0530

    use linq where<> #01

--- LINQ Library/Program.cs
+++ LINQ Library/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+var words = new List<string>
+{
+    "a","bb","ccc","dddd","eeeee"
+};
+
+var wordLongerThenTwoLatter = words.Where(word => word.Length > 2);
+
+foreach (var item in wordLongerThenTwoLatter)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+var number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+var oddNumbers = number.Where(numb => numb % 2 == 1);
+
+foreach (var item in oddNumbers)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
