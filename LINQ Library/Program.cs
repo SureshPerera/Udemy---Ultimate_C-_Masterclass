9abe1a2644a490b39c491c39c8b627c40541cf05
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-01-22 11:22:12 +0530

    using Any<> #02

--- LINQ Library/Program.cs
+++ LINQ Library/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Channels;
+
 var words = new List<string>
 {
     "a","bb","ccc","dddd","eeeee"
@@ -11,11 +13,36 @@ foreach (var item in wordLongerThenTwoLatter)
     Console.WriteLine(item);
 }
 Console.WriteLine();
-var number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-var oddNumbers = number.Where(numb => numb % 2 == 1);
+var number = new int[] { 7,10, 50,80,1, 2, 3, 4, 5, 6, 8 };
 
-foreach (var item in oddNumbers)
+var PetDetails = new List<Pet>
 {
-    Console.WriteLine(item);
-}
+    new Pet(1,"sampath",3.5f),
+    new Pet(2,"kamal",3.2f),
+    new Pet(3,"sudeera",6.3f),
+    new Pet(4,"suranga",7.3f),
+    new Pet(5,"pradeep",9.3f),
+    new Pet(6,"sampath1",13f),
+    new Pet(7,"sampath5",1.5f),
+};
+
+var isThereAnyCollection = PetDetails.Any(pet => pet.Name == "sampath");
+
+Console.WriteLine(string.Join(",",isThereAnyCollection));
+
 Console.ReadLine();
+
+public class Pet
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Hight { get; set; }
+    
+    public Pet(int id,string name,double hight)
+    {
+        Id = id;
+        Name = name;
+        Hight = hight;
+    }
+
+}
