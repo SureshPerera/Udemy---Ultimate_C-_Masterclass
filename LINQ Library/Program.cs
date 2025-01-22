e76b5822da6320f2a7a2e1b79e2334c5c85c208f
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-01-22 20:22:30 +0530

    using Select<> #08

--- LINQ Library/Program.cs
+++ LINQ Library/Program.cs
@@ -11,6 +11,9 @@ var wordLongerThenTwoLatter = words.Where(word => word.Length > 2);
 
 var number = new int[] { 7,10, 10,80,1, 2, 2, 4, 5, 6, 8 };
 
+var sumNumber = number.Select(sum => sum * sum);
+
+//Console.WriteLine(string.Join(",",sumNumber));
 var removeRepeatItem = number.Distinct();
 
 //Console.WriteLine(string.Join(",",removeRepeatItem));
@@ -32,12 +35,27 @@ var PetDetails = new List<Pet>
     new Pet(7,"sampath5",1.5f),
     new Pet(8,"",2.5f)
 };
+var upperCase = PetDetails.Select(pet => pet.Name.ToUpper()).ToList();
+foreach (var name in PetDetails)
+{
+    //Console.WriteLine(name.Name);
+
+}
+
+var list = new string[] {"sures","gamage" };
+
+var toUpperLetter = list.Select(lis => lis.ToUpper());
+foreach(var name in toUpperLetter)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine();
 
 var removeItems = PetDetails.DistinctBy(pets => pets.Name).ToList();
 
 foreach(var item in removeItems){
 
-Console.WriteLine(item.Name);
+
 }
 var petColletionByPetName = PetDetails.OrderByDescending(pets => pets.Name).ThenBy(pet => pet.Hight);
 
