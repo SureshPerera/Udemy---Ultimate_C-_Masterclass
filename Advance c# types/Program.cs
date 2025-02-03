c9880da1b6a2cde5f2fb1f6b39a02313d7c2f1a2
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-02-03 22:24:14 +0530

    use with { }

--- Advance c# types/Program.cs
+++ Advance c# types/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿var structclass = new number(10,15);
+
+var news = structclass with { Num1 = 1, Num2 = 5 };
+
+Console.WriteLine(news);
+
+Console.ReadLine();
+
+public struct number
+{
+    public int Num1 { get; set; }
+    public int Num2 { get; set; }
+
+    public number(int num1, int num2)
+    {
+        Num1 = num1;
+        Num2 = num2;
+    }
+    public override string ToString() => $"{Num1} {Num2}";  
+}
+
+public class numbers
+{
+    public int Num1 { get; set; }
+    public int Num2 { get; set;  }
+
+    public numbers(int num1, int num2)
+    {
+        Num1 = num1;
+        Num2 = num2;
+    }
+
+    public override string ToString() => $"{Num1} {Num2}";
+}
