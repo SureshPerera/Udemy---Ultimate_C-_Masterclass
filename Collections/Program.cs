888b73ecd52edf4674d77a51c884fa773a16d0c9
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-02-23 23:23:27 +0530

    update ICollection

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -70,73 +70,88 @@
 //}
 
 
-using System.Collections;
-using System.Security;
-var list = new List<string>
-{
-    "kamal","addarasinha"
-};
+//using System.Collections;
+//using System.Security;
+//var list = new List<string>
+//{
+//    "kamal","addarasinha"
+//};
+
+//foreach (var item in list)
+//{
+//    Console.WriteLine(item);
+
+//}
+
+//Console.ReadLine();
+//public class EmployerName : IEnumerable
+//{
+//    public List<string> list = new List<string>();
+
+//    public EmployerName(List<string> List)
+//    {
+//        list = List;
+//    }
+//    public IEnumerator  GetEnumerator()
+//    {
+//        new Enumerator(list);
+//        return list.GetEnumerator();
+
+//    }
+//}
+//public class Enumerator : IEnumerator
+//{
+//    public const int InitialPossitions = -1;
+//    public int currentPositions = InitialPossitions;
+//    public List<string> list = new List<string>();
+
+//    public Enumerator(List<string> List)
+//    {
+//        list = List;
+//    }
+//    public object Current
+//    {
+//        get
+//        {
+//            try
+//            {
+//                return list[currentPositions];
+
+//            }
+//            catch (IndexOutOfRangeException ex)
+//            {
+
+//                throw new IndexOutOfRangeException($"{nameof(EmployerName)}'s end reached", ex);
+
+//            }
+//        }
+//    }
+
+//    public bool MoveNext()
+//    {
+//        ++currentPositions;
+//        return currentPositions < list.Count;
+//    }
+
+//    public void Reset()
+//    {
+//        currentPositions = 0;
+//    }
+//}
+
+//Icillections //////////////////////////////////////////////////////////
+
+var array = new int[] { 5, 8, 4 };
+ICollection<int> list = new List<int> { 50,100};
+IReadOnlyCollection<int> collectionss = new int[] { 5, 8, 4 };
+
+collectionss
+list.Add(50);
+
 
 foreach (var item in list)
 {
     Console.WriteLine(item);
-
 }
-
+Console.WriteLine();
 Console.ReadLine();
-public class EmployerName : IEnumerable
-{
-    public List<string> list = new List<string>();
-
-    public EmployerName(List<string> List)
-    {
-        list = List;
-    }
-    public IEnumerator  GetEnumerator()
-    {
-        new Enumerator(list);
-        return list.GetEnumerator();
-          
-    }
-}
-public class Enumerator : IEnumerator
-{
-    public const int InitialPossitions = -1;
-    public int currentPositions = InitialPossitions;
-    public List<string> list = new List<string>();
-
-    public Enumerator(List<string> List)
-    {
-        list = List;
-    }
-    public object Current
-    {
-        get
-        {
-            try
-            {
-                return list[currentPositions];
-
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-
-                throw new IndexOutOfRangeException($"{nameof(EmployerName)}'s end reached", ex);
-
-            }
-        }
-    }
-
-    public bool MoveNext()
-    {
-        ++currentPositions;
-        return currentPositions < list.Count;
-    }
-
-    public void Reset()
-    {
-        currentPositions = 0;
-    }
-}
-
-
